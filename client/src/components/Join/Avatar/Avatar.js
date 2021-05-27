cb29fa4aa49ad258110cd7a0d390bb88f2934ab5
Cappuu <w8237228@icloud.com>
2021-05-27 22:51:49 +0800

    修正 react warning

--- client/src/components/Join/Avatar/Avatar.js
+++ client/src/components/Join/Avatar/Avatar.js
@@ -1,43 +1,44 @@
 import React from 'react';
-
 import './Avatar.scss';
 import avatars from '../../Image/AvatarImage';
 
-const AvatarContent = ({i,getAvatar,avatar}) => {
-    return (
-        <li>
-            <input type="radio" name="avatar" id={`avatar${i+1}`} value={i} onChange={ getAvatar }/>
-            <label htmlFor={`avatar${i+1}`} style={{ backgroundImage:`url(${avatar})` }}>
-            </label>
-        </li>
-    )
+const AvatarContent = ({ i, getAvatar, avatar }) => {
+  return (
+    <li>
+      <input type="radio" name="avatar" id={`avatar${i + 1}`} value={i} onChange={getAvatar} />
+      <label htmlFor={`avatar${i + 1}`} style={{ backgroundImage: `url(${avatar})` }}>
+      </label>
+    </li>
+  )
 }
 const Avatar = ({ getAvatar }) => {
-    return(
-        <div className="avatar-wrapper">
-            <ul>
-            {
-                avatars.map((avatar,i)=>{
-                    if(i >= 4) return ;
-                    return(
-                        <AvatarContent i={i} getAvatar={getAvatar} avatar={avatar} key={avatar}/>
-                    )
-                })
-            }
-            </ul>
-            <ul>
-            {
-                avatars.map((avatar,i)=>{
-                    if(i <=3 ) return ;
-                    return(
-                        <AvatarContent i={i} getAvatar={getAvatar} avatar={avatar} key={avatar}/>
-                    )
-                })
-            }
-            </ul>
-        </div>
-    )
-    
+  return (
+    <div className="avatar-wrapper">
+      <ul>
+        {
+          avatars
+            .filter((avatar, i) => i < 4)
+            .map((avatar, i) => {
+              return (
+                <AvatarContent i={i} getAvatar={getAvatar} avatar={avatar} key={avatar} />
+              )
+            })
+        }
+      </ul>
+      <ul>
+        {
+          avatars
+            .filter((avatar, i) => i >= 4)
+            .map((avatar, i) => {
+              return (
+                <AvatarContent i={i} getAvatar={getAvatar} avatar={avatar} key={avatar} />
+              )
+            })
+        }
+      </ul>
+    </div>
+  )
+
 }
 
-export default Avatar ;
+export default Avatar;
