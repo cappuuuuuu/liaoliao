06dbd57bbe67d4c50054a8b562bc7aed97f1c2d0
cappuuuuuu <w1814946281@gmail.com>
2021-09-07 01:31:27 +0800

    修正 環境變數型別為 Object

--- client/src/helper/index.js
+++ client/src/helper/index.js
@@ -2,6 +2,6 @@ export function getConfigsJSON () {
   try {
     return require('@/configs/index.json')
   } catch (error) {
-    return process.env.SERVER_ORIGIN
+    return { SERVER_ORIGIN: process.env.SERVER_ORIGIN }
   }
 }
